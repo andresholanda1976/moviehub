00da49812cbac6098074766436ab21151c3f23af
Andre Holanda <andre.s.holanda@gmail.com>
2026-05-23 19:37:08 -0300

    CorrecaoAoIncluirDuracao

--- src/br/com/cdl/moviehub/calculos/CalculadoraDeTempo.java
+++ src/br/com/cdl/moviehub/calculos/CalculadoraDeTempo.java
@@ -20,7 +20,8 @@ public class CalculadoraDeTempo {
 //    }
 
 public void inclui(Titulo titulo) {
-    System.out.println("Adicionando duracao em minutos de " + titulo);
+    System.out.println("Adicionando duracao em minutos de " + titulo.getNome()
+);
     this.tempoTotal += titulo.getDuracaoEmMinutos();
 }
 
